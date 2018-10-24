00aa395f787f79a24ecedad6facfe6ca48bf241a
Peter-Simon Dieterich <peter.dieterich@ek-team.de>
2018-10-24 10:39:15 +0200

    New Chatbird class

---
+++ chatbird.py
@@ -0,0 +1,73 @@
+from time import sleep
+from gtts import gTTS
+from pydub import AudioSegment
+
+from fcntl import ioctl
+import logging
+
+CHATBIRD_FLAP = ord('3')
+CHATBIRD_BEAK = ord('4')
+CHATBIRD_RESET = ord('6')
+CHATBIRD_TILT = ord('5')
+CHATBIRD_OFF = ord('1')
+
+class Chatbird:
+    def __init__(self, device):
+        self.logger = logging.getLogger("Chatbird")
+        self._device = device
+
+    def __enter__(self):
+        self._fd = open(self._device, 'wb')
+        return ChatbirdInterface(self._fd)
+
+    def __exit__(self, *args):
+        self._fd.close()
+
+
+class ChatbirdInterface:
+    def __init__(self, fd):
+        self._fd = fd
+
+    def _send_command(self, command):
+        ioctl(self._fd, 0, command, False)
+        self._fd.flush()
+
+    def _send_data(self, data):
+        self._fd.write(data)
+        self._fd.flush()
+
+    def flap(self):
+        self._send_command(CHATBIRD_FLAP)
+
+    def beak(self):
+        self._send_command(CHATBIRD_BEAK)
+
+    def reset(self):
+        self._send_command(CHATBIRD_RESET)
+
+    def tilt(self):
+        self._send_command(CHATBIRD_TILT)
+        sleep(1)
+        self.reset()
+
+    def off(self):
+        self._send_command(CHATBIRD_OFF)
+
+    def say(self, text, lang='en'):
+        tts = gTTS(text, lang=lang)
+        # TODO: use write_to_fd instead
+        tts.save('tmp.mp3')
+
+        sound = AudioSegment.from_mp3('tmp.mp3')
+        sound = sound.set_frame_rate(12000)
+        # make quieter by amount in dB
+        sound = sound - 15
+
+        self.flap()
+        sleep(2)
+        self.beak()
+        sleep(0.3)
+        self._send_data(sound.raw_data)
+        self.flap()
+        sleep(1)
+        self.off()
--- telegrambot.py
+++ telegrambot.py
@@ -8,97 +8,26 @@ import logging
 from telegram.ext.filters import Filters
 from telegram.ext import Updater, CommandHandler, MessageHandler
 
+from chatbird import Chatbird
 from secrets import TOKEN
+
+CHATBIRD_DEVICE = '/dev/cb1'
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                              level=logging.INFO)
 
-
-CHATBIRD_FLAP = ord('3')
-CHATBIRD_BEAK = ord('4')
-CHATBIRD_RESET = ord('6')
-CHATBIRD_TILT = ord('5')
-CHATBIRD_OFF = ord('1')
-
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
-
-def exec_command(f, command):
-    ioctl(f, 0, command, False)
-    f.flush()
-
-def exec_speak(f, data):
-    f.write(data)
-    f.flush()
 
 def say(bot, update):
     text = update.message.text
     print("Message received {}".format(text))
 
-    tts = gTTS(text, lang='de')
-    tts.save('tmp.mp3')
-
-    sound = AudioSegment.from_mp3('tmp.mp3')
-    sound = sound.set_frame_rate(12000)
-    # make quieter by amount in dB
-    sound = sound - 15
-
-    f = open("/dev/cb1", "wb")
-    exec_command(f, CHATBIRD_FLAP)
-    sleep(2)
-    exec_command(f, CHATBIRD_BEAK)
-    sleep(0.3)
-    exec_speak(f, sound.raw_data)
-    #sleep(sound.duration_seconds + 1)
-    exec_command(f, CHATBIRD_FLAP)
-    sleep(1)
-    exec_command(f, CHATBIRD_OFF)
-    f.close()
+    with Chatbird(CHATBIRD_DEVICE) as cb:
+        cb.say(text, 'de')
 
 message_handler = MessageHandler(Filters.text, say)
 #say_handler = CommandHandler('say', say)
 dispatcher.add_handler(message_handler)
 
 updater.start_polling()
-
-# f = open("/dev/cb1", "wb")
-
-# try:
-#     inkey = _Getch()
-#     while (1):
-#         print("""
-#         1 - all off
-#         2 - LEDs on
-#         3 - Flap wings
-#         4 - Move beak
-#         5 - Tilt head
-#         6 - Reset wings and head
-#         s - Read text (English)
-#         g - Read text (German)
-
-#         q - Quit
-#         """)
-#         key = input("Command: ")
-#         # key = inkey()
-#         if key == "q": break
-#         if key == "s" or key == "g":
-#             text = input("Input text: ")
-#             tts = gTTS(text, lang='en' if key == "s" else 'de')
-#             tts.save('tmp.mp3')
-
-#             sound = AudioSegment.from_mp3('tmp.mp3')
-#             sound = sound.set_frame_rate(12000)
-#             # make quieter by amount in dB
-#             sound = sound - 15
-#             f.write(sound.raw_data)
-#             f.flush()
-#             continue
-#         print(key)
-#         key = key[0]
-#         # f.write(key)
-#         ioctl(f, 0, ord(key), False)
-#         f.flush()
-# except (KeyboardInterrupt, SystemExit):
-#     print("Exiting")
-
-# f.close()
